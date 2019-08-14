3fbbc11ac0ef5740f1dfa697d2bb2870b7a0fb4e
Marin <marinvarnica@gmail.com>
2019-08-14 14:36:49 +0200

    added login controller

---
+++ app/Controllers/Login.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Controllers;
+
+
+use Core\Controller;
+
+class Login extends Controller
+{
+    public function index()
+    {
+        $this->view('Login' . DIRECTORY_SEPARATOR . 'index');
+        $this->view->render();
+    }
+}
