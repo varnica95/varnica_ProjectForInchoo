52a3d3aa2cd20bf51c6fe15b49b903dbe9aa2a7c
Marin <marinvarnica@gmail.com>
2019-08-14 23:08:24 +0200

    fixed some bugs

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,11 +16,17 @@ class User extends Database
 
     public function newUser()
     {
-        var_dump($this->username);
         $conn = Database::getInstance()->getPDO();
-        $sql = 'INSERT INTO users(fname, lname, email, username, pwd) VALUES (?, ?, ?, ?, ?)';
+
+        $sql = 'INSERT INTO users(fname, lname, email, username, pwd) VALUES (:firstname, :lastname, :email, :username, :pwd)';
         $stmt = $conn->prepare($sql);
 
-        $stmt->execute([$this->firstname, $this->lastname, $this->email, $this->username, $this->pwd]);
+        $stmt->bindValue(':firstname', $this->firstname);
+        $stmt->bindValue(':lastname', $this->lastname);
+        $stmt->bindValue(':email', $this->email);
+        $stmt->bindValue(':username', $this->username);
+        $stmt->bindValue(':pwd', $this->pwd);
+
+        $stmt->execute();
     }
 }
